59910ac13b6d9cc34d27b947a7f4fcf0a9b8a587
Jens Brokfeld <geofreak.de@gmail.com>
2020-09-11 12:47:03 +0200

    whitelist

--- src/whitelist.js
+++ src/whitelist.js
@@ -3,5 +3,6 @@ const whitelist = [];
 whitelist.push(`fewo-direkt.de`);
 whitelist.push(`www.fewo-direkt.de`);
 whitelist.push(`www.googleadservices.com`);
+whitelist.push(`googleadservices.com`);
 
 module.exports = whitelist;
