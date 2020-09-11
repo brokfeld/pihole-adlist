a13fdfa8f91a7860fa080c6e12b1b8de7a4ca125
Jens Brokfeld <geofreak.de@gmail.com>
2020-09-11 16:21:45 +0200

    Allow google ads

--- src/whitelist.js
+++ src/whitelist.js
@@ -6,5 +6,6 @@ whitelist.push(`www.fewo-direkt.de`);
 // Google Ads
 whitelist.push(`doubleclick.net`);
 whitelist.push(`googleadservices.com`);
+whitelist.push(`dartsearch.net`);
 
 module.exports = whitelist;
--- src/whitelistEndsWith.js
+++ src/whitelistEndsWith.js
@@ -3,5 +3,6 @@ const whitelistEndsWith = [];
 // Google Ads
 whitelistEndsWith.push(`.doubleclick.net`);
 whitelistEndsWith.push(`.googleadservices.com`);
+whitelist.push(`.dartsearch.net`);
 
 module.exports = whitelistEndsWith;
